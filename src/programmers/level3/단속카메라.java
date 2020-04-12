c5a4ff757a82401f815d4f19a384a68c26c06a58
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2020-04-12 10:25:16 +0900

    단속카메라

---
+++ src/programmers/level3/단속카메라.java
@@ -0,0 +1,32 @@
+package programmers.level3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import org.junit.Assert;
+import org.junit.Test;
+
+// https://programmers.co.kr/learn/courses/30/lessons/42884
+
+public class 단속카메라 {
+    public int solution(int[][] routes) {
+      int answer = 0;
+      int place = -30001;
+
+      Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
+
+      for (int[] route : routes) {
+        if (place < route[0]) {
+          place = route[1];
+          answer++;
+        }
+      }
+
+      return answer;
+    }
+
+  @Test
+  public void 정답(){
+    Assert.assertEquals(2, solution(new int[][]{{-20,15}, {-14,-5}, {-18,-13}, {-5,-3}}));
+  }
+
+}
