b567729f743a7a696d98b916de6823da6f0e9728
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2020-04-18 21:47:56 +0900

    등굣길

--- src/programmers/level3/등굣길.java
+++ src/programmers/level3/등굣길.java
@@ -5,31 +5,31 @@ import org.junit.Test;
 
 public class 등굣길 {
   public int solution(int m, int n, int[][] puddles) {
-    int[][] street = new int[n + 1][m + 1];
+    int[][] street = new int[n][m];
 
     // 웅덩이는 -1
     for (int[] puddle : puddles)
-      street[puddle[1]][puddle[0]] = -1;
+      street[puddle[1] - 1][puddle[0] - 1] = -1;
 
-    street[1][1] = 1;
+    street[0][0] = 1;
 
-    for (int i = 1; i <= n; i++) { // 시작점은 1로 저장
-      for (int j = 1; j <= m; j++) {
+    for (int i = 0; i < n; i++) { // 시작점은 1로 저장
+      for (int j = 0; j < m; j++) {
 
         if(street[i][j] == -1) {
           street[i][j] = 0;
           continue;
         }
 
-        if(i != 1)
+        if(i != 0)
           street[i][j] += street[i - 1][j] % 1000000007; // 위
 
-        if(j != 1)
+        if(j != 0)
           street[i][j] += street[i][j - 1] % 1000000007; // 왼쪽
       }
     }
 
-    return street[n][m] % 1000000007;
+    return street[n - 1][m - 1] % 1000000007;
   }
 
   @Test
