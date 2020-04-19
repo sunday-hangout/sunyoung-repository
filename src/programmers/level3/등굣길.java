0ea3cc90788afcbd6165eb5a9bab276ae96b17e9
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2020-04-19 18:49:43 +0900

    등굣길

--- src/programmers/level3/등굣길.java
+++ src/programmers/level3/등굣길.java
@@ -16,13 +16,13 @@ public class 등굣길 {
     for (int i = 0; i < n; i++) { // 시작점은 1로 저장
       for (int j = 0; j < m; j++) {
 
-        if(street[i][j] == -1) {
+        if(street[i][j] == -1) { // 웅덩이면 0으로 바꾸고 continue
           street[i][j] = 0;
           continue;
         }
 
         if(i != 0)
-          street[i][j] += street[i - 1][j] % 1000000007; // 위
+          street[i][j] += street[i - 1][j] % 1000000007; // 숫자가 이 값을 초과할 수 있기 때문에 계산 과정에서 나머지 구하기
 
         if(j != 0)
           street[i][j] += street[i][j - 1] % 1000000007; // 왼쪽
