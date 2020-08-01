ca2311a49d106cee8981b1a2f1b10b8ba2e9c1ee
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2020-08-01 20:56:09 +0900

    123 더하기

---
+++ src/baekjoon/silver/_123_더하기.java
@@ -0,0 +1,30 @@
+package baekjoon.silver;
+
+import java.util.Scanner;
+
+public class _123_더하기 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int num = sc.nextInt();
+    int n;
+
+    int[] output = new int[11];
+    output[1] = 1; // 1
+    output[2] = 2; // 1+1, 2
+    output[3] = 4; // 1+1+1, 1+2, 2+1, 3
+
+    for (int i = 0; i < num; i++) {
+      n = sc.nextInt();
+
+      for (int j = 4; j <= n; j++) {
+        output[j] = output[j - 1] + output[j - 2] + output[j - 3];
+      }
+      System.out.println(output[n]);
+    }
+
+
+  }
+
+}
